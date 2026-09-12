5f9132098a8e6fd3538e03647a57975352cd2f80
wotaku-ship-it <wotaku2323@gmail.com>
2026-09-12 00:40:36 -0400

    Update wtrlabes.ts

--- plugins/spanish/wtrlabes.ts
+++ plugins/spanish/wtrlabes.ts
@@ -8,7 +8,7 @@ class WTRLAB implements Plugin.PluginBase {
   id = 'WTRLAB';
   name = 'WTR-LAB';
   site = 'https://wtr-lab.com/';
-  version = '1.1.6';
+  version = '1.0.0';
   icon = 'src/es/wtrlab/icon.png';
   sourceLang = 'es/';
   baggage = '';
